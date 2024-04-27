a8d62bdf4ac8cb187578dce0c06d96668ed0ea39
Rafa <rafaell.rochaa@gmail.com>
2024-04-26 21:14:15 -0300

    update ignoring.

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('C:\\Users\\Rafa\\Documents\\projetos pessoais\\Portifolio-Brandao-main\\node_modules\\infima\\dist\\css\\default\\default.css'),
+  require('C:\\Users\\Rafa\\Documents\\projetos pessoais\\Portifolio-Brandao-main\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages'),
+  require('C:\\Users\\Rafa\\Documents\\projetos pessoais\\Portifolio-Brandao-main\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress'),
+  require('C:\\Users\\Rafa\\Documents\\projetos pessoais\\Portifolio-Brandao-main\\src\\css\\custom.css'),
+];
